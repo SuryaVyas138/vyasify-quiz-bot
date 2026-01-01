4ebcc0321d7f7d866a1064cecba910e76b8850ba
SuryaVyas138 <surya.suryateja999@gmail.com>
2026-01-01 18:02:53 +0530

    Add main bot file

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import os
+from telegram.ext import ApplicationBuilder, CommandHandler
+from telegram import Update
+from telegram.ext import ContextTypes
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "📘 Welcome to Vyasify Quiz Bot\n\n"
+        "Cloud version is live and stable."
+    )
+
+def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
 
